d62d296a662f7b64443ab3f6dbe1e169dd8d6f73
A1g0S <95964570+A1g0S@users.noreply.github.com>
2022-01-06 11:48:45 +0200

    Update torftp.py

--- torftp.py
+++ torftp.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
        sys.exit()
     
     print(logged)
-   # try:
+    try:
     while True:
       print(recvall(s))
       cwd=pwd(s)
@@ -228,6 +228,6 @@ if __name__ == "__main__":
       else:
         sendall(s,cmd)
         print(recvall(s))
-   # except:
-    #  s.close()
-     # sys.exit()
+    except:
+      s.close()
+      sys.exit()
